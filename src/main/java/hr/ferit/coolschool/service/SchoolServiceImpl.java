779ae6a9b3ba726e5468c1be9f994902dd0a1178
skristijan <stef.kristijan@gmail.com>
2018-09-08 19:06:05 +0200

    Added endpoint for list of users in a school

--- src/main/java/hr/ferit/coolschool/service/SchoolServiceImpl.java
+++ src/main/java/hr/ferit/coolschool/service/SchoolServiceImpl.java
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 @Service
@@ -36,20 +37,25 @@ public class SchoolServiceImpl implements SchoolService {
 
     @Override
     public List<User> findAllUsersInSchool(Integer schoolId, boolean teachersOnly, boolean studentsOnly) {
-        School school = this.schoolRepository.findById(schoolId)
+        this.schoolRepository.findById(schoolId)
                 .orElseThrow(() -> new ResourceNotFoundException("Tražena škola ne postoji"));
 
         if (teachersOnly && studentsOnly || !teachersOnly && !studentsOnly) {
             return this.userSchoolRepository.findAllBySchoolSchoolId(schoolId).stream()
-                    .map(UserSchool::getUser).collect(Collectors.toList());
-        }else if(studentsOnly){
-            System.out.println("Students only");
+                    .map(removeSchoolsForMap()).collect(Collectors.toList());
+        } else if (studentsOnly) {
             return this.userSchoolRepository.findAllBySchoolSchoolIdAndUserRole(schoolId, Role.ROLE_STUDENT).stream()
-                    .map(UserSchool::getUser).collect(Collectors.toList());
-        }else{
-            System.out.println("Teachers only");
+                    .map(removeSchoolsForMap()).collect(Collectors.toList());
+        } else {
             return this.userSchoolRepository.findAllBySchoolSchoolIdAndUserRole(schoolId, Role.ROLE_TEACHER).stream()
-                    .map(UserSchool::getUser).collect(Collectors.toList());
+                    .map(removeSchoolsForMap()).collect(Collectors.toList());
         }
+    }
+
+    private Function<UserSchool, User> removeSchoolsForMap() {
+        return us -> {
+            us.getUser().setUserSchools(null);
+            return us.getUser();
+        };
     }
 }
